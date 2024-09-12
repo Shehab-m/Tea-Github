8363371406e7eae3961731d7e46c9eccce432dd9
Shehab Mohammed Mohammed <90576202+Shehab-m@users.noreply.github.com>
2024-09-12 04:36:31 +0300

    provide local composition in main activity use appNavHost in main activity

--- app/src/main/java/com/teacomputers/teagithub/MainActivity.kt
+++ app/src/main/java/com/teacomputers/teagithub/MainActivity.kt
@@ -1,47 +1,30 @@
 package com.teacomputers.teagithub
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.teacomputers.teagithub.presentation.navigation.AppNavHost
+import com.teacomputers.teagithub.presentation.navigation.LocalNavigationProvider
 import com.teacomputers.teagithub.ui.theme.TeaGithubTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            TeaGithubTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            CompositionLocalProvider(LocalNavigationProvider provides rememberNavController()) {
+                TeaGithubTheme {
+                    Scaffold(modifier = Modifier.fillMaxSize()) {
+                        AppNavHost()
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TeaGithubTheme {
-        Greeting("Android")
     }
 }
