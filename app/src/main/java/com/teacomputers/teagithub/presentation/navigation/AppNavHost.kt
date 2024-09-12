62a25a514878fa92a7cdce7354d13e42b9b5979d
Shehab Mohammed Mohammed <90576202+Shehab-m@users.noreply.github.com>
2024-09-12 04:28:15 +0300

    create nav host controller create appNavHost

---
+++ app/src/main/java/com/teacomputers/teagithub/presentation/navigation/AppNavHost.kt
@@ -0,0 +1,19 @@
+package com.teacomputers.teagithub.presentation.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+
+@Composable
+fun AppNavHost() {
+    val navController = LocalNavigationProvider.current
+    NavHost(
+        navController = navController,
+        startDestination = Graph.AUTH
+    ) {
+        authNavGraph()
+    }
+}
+
+object Graph {
+    const val AUTH = "auth_graph"
+}
